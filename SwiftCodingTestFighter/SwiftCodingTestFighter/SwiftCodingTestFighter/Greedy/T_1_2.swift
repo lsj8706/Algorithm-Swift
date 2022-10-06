fe0fe67ad11eac693c3ff2d7006280b4f75654e7
Sejin Lee <lsj8706@naver.com>
2022-10-06 21:26:56 +0900

    [ADD] : 그리디 기출 #2 곱하기 혹은 더하기

--- SwiftCodingTestFighter/SwiftCodingTestFighter/SwiftCodingTestFighter/Greedy/T_1_2.swift
+++ SwiftCodingTestFighter/SwiftCodingTestFighter/SwiftCodingTestFighter/Greedy/T_1_2.swift
@@ -10,5 +10,17 @@
 import Foundation
 
 func solveT1_2() {
-    print("sad")
+    let input = readLine()!.map{ Int(String($0))! }
+    var res = input[0]
+    
+    for i in 1..<input.count {
+        let num = input[i]
+        if num < 1 || res < 1 {
+            res += num
+        } else {
+            res *= num
+        }
+    }
+    
+    print(res)
 }
