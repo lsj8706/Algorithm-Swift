9fd69b07333008ff219cd7d850a95c952f2e53bd
Sejin Lee <lsj8706@naver.com>
2023-05-13 13:49:06 +0900

    [FIX] : pg 131704 택배상자 (시간 초과 해결)

--- Programmers/programmers/programmers/Level2/pg_131704.swift
+++ Programmers/programmers/programmers/Level2/pg_131704.swift
@@ -15,31 +15,34 @@ func solve131704() {
 
 fileprivate func solution(_ order:[Int]) -> Int {
     var stack = [Int]() // 보조 컨테이너 벨트
-    var order = order
+    var order = Array(order.reversed())
     var result = 0
     let totalBoxCnt = order.count
     
-    var i = 1 // 들어오는 박스 넘버
+    var i = 1 // 주 컨테이터에서 들어오는 박스 넘버
     
     while !order.isEmpty && i <= totalBoxCnt+1 {
-        let needToStore = order.first!
+        let needToStore = order.last!
         // 주 컨테이너 또는 보조 컨테이너에 해당 번호가 있을 경우 result 증가
         if i == needToStore {
             result += 1
-            order.removeFirst()
+            order.removeLast()
             i += 1
             continue
         } else if !stack.isEmpty && stack.last! == needToStore {
             result += 1
             stack.removeLast()
-            order.removeFirst()
+            order.removeLast()
             continue
         }
         
-        // 둘 다에서 못찾을 경우 보조 컨터이너 (stack)에 추가
+        // 둘 다에서 못찾을 경우 보조 컨테이너 (stack)에 추가
         stack.append(i)
         i += 1
     }
     
     return result
 }
+
+// order를 그대로 사용하면 removeFirst를 사용해야 하는데 이 함수의 시간복잡도가 O(n)이라 시간 초과가 발생한다.
+// 따라서 order를 reversed()하여 스택으로 바꾼 후 removeLast를 사용하는 방식으로 시간 초과 문제를 해결했다.
