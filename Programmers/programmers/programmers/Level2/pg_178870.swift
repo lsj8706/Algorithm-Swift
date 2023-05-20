98b70635451ba09b9391d8d15e3aeda7ebca4bcd
Sejin Lee <lsj8706@naver.com>
2023-05-20 15:54:49 +0900

    [ADD] : pg 178870 연속된 부분 수열의 합 (투포인터)

--- Programmers/programmers/programmers/Level2/pg_178870.swift
+++ Programmers/programmers/programmers/Level2/pg_178870.swift
@@ -10,49 +10,45 @@
 import Foundation
 
 func solve178870() {
-    print(solution([2, 2, 2, 2, 2], 6))
+    print(solution([1, 2, 3, 4, 5], 7))
 }
 
 fileprivate func solution(_ sequence:[Int], _ k:Int) -> [Int] {
     
-    var result = [Int]()
-    var len = 1 // 부분 수열의 길이
+    var result = [0, sequence.count - 1]
+    // 투 포인터
+    var start = 0
+    var end = 0
     let count = sequence.count
+    var sum = sequence[0]
     
-    // sequence에서 start 인덱스부터 end 인덱스까지의 합을 저장할 2차원 배열 (dp)
-    var memory = Array(repeating: Array(repeating: 0, count: count), count: count)
-    
-    func getSumAtMemory(start: Int, end: Int) -> Int {
-        var sum = memory[start][end]
+    while start >= 0 && end < count && start <= end {
         
-        if sum == 0 { // memory에서 못 찾은 경우
-            if end - 1 >= 0 {
-                sum = memory[start][end-1] + sequence[end]
-            } else {
-                sum = sequence[start]
+        if sum == k {
+            if end - start < result[1]-result[0] {
+                result = [start, end]
             }
-            memory[start][end] = sum
-            return sum
-        }
-        
-        return sum
-    }
-    
-    // 부분 수열의 길이를 1부터 증가시키면서 찾는다
-    outer: while len <= sequence.count {
-        for i in 0..<sequence.count {
-            if i+len > sequence.count { break }
-            let sum = getSumAtMemory(start: i, end: i+len-1)
-
-            if sum > k { break }
             
-            if sum == k {
-                result = [i, i+len-1]
-                break outer
+            if end+1 < count {
+                end += 1
+                sum += sequence[end]
+            } else {
+                break
             }
+            continue
         }
         
-        len += 1
+        if sum < k {
+            if end+1 < count {
+                end += 1
+                sum += sequence[end]
+            } else {
+                break
+            }
+        } else if sum > k {
+            sum -= sequence[start]
+            start += 1
+        }
     }
     
     return result
