8f3296b96ef581ad5259a7e7f98410bed26af7bf
Sejin Lee <lsj8706@naver.com>
2023-06-20 23:58:24 +0900

    [ADD] : pg 135807 숫자 카드 나누기 (시간 초과 해결)

--- Programmers/programmers/programmers/Level2/pg_135807.swift
+++ Programmers/programmers/programmers/Level2/pg_135807.swift
@@ -10,17 +10,35 @@
 import Foundation
 
 func solve135807() {
-    print(solution([10, 20], [5, 17]))
+    print(solution([10, 20], [5]))
 }
 
 fileprivate func solution(_ arrayA:[Int], _ arrayB:[Int]) -> Int {
+    var result = 0
     let arrayA = arrayA.sorted()
     let arrayB = arrayB.sorted()
     
-    var result = 0
+    // 각 어레이의 최솟값의 약수들
+    var aSubMultiples = Set<Int>()
+    var bSubMultiples = Set<Int>()
 
-    // A에서 가장 작은 수까지의 약수들로 비교 (가장 작은수보다 커지면 나누는 것 불가)
-    outer: for i in stride(from: arrayA[0], through: 2, by: -1) {
+    for i in 1...arrayA[0] {
+        if arrayA[0] % i == 0 {
+            aSubMultiples.insert(i)
+        }
+    }
+    
+    for i in 1...arrayB[0] {
+        if arrayB[0] % i == 0 {
+            bSubMultiples.insert(i)
+        }
+    }
+    
+    // 약수가 중복되면 해당 약수로는 정답이 될 수 없기 때문에 차집합으로 빼준다.
+    aSubMultiples = aSubMultiples.subtracting(bSubMultiples)
+    bSubMultiples = bSubMultiples.subtracting(aSubMultiples)
+
+    outer: for i in aSubMultiples.sorted(by: >) {
         
         for a in arrayA {
             if a % i != 0 {
@@ -38,8 +56,7 @@ fileprivate func solution(_ arrayA:[Int], _ arrayB:[Int]) -> Int {
         break
     }
     
-    // B에서 가장 작은 수까지의 약수들로 비교 (가장 작은수보다 커지면 나누는 것 불가)
-    outer: for i in stride(from: arrayB[0], through: 2, by: -1) {
+    outer: for i in bSubMultiples.sorted(by: >) {
         
         if i <= result { break }
         
