5085b4adc6c395764ce9c021549d73cde1bac0a2
Sejin Lee <lsj8706@naver.com>
2023-06-29 19:34:14 +0900

    [FIX] : pg 159993 미로 탈출 (시간 초과 해결)

--- Programmers/programmers/programmers/Level2/pg_159993.swift
+++ Programmers/programmers/programmers/Level2/pg_159993.swift
@@ -10,81 +10,65 @@
 import Foundation
 
 func solve159993() {
-    print(solution(["LOOXS","OOOOX","OOOOO","OOOOO","EOOOO"]))
+    print(solution(["SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"]))
 }
 
 fileprivate func solution(_ maps:[String]) -> Int {
-    let maps = maps.map { Array($0).map { String($0) } }
+    var mapArr: [[Character]] = []
     
     let dx = [1, -1, 0, 0]
     let dy = [0, 0, 1, -1]
     
-    let visited1 = Array(repeating: Array(repeating: false, count: maps[0].count), count: maps.count)
-    let visited2 = Array(repeating: Array(repeating: false, count: maps[0].count), count: maps.count)
+    var startCoord: (x: Int, y: Int) = (0, 0)
+    var leverCoord: (x: Int, y: Int) = (0, 0)
     
-    func bfs(x: Int, y: Int, target: String, visited: [[Bool]]) -> Int {
-        var visited = visited
-        var cnt = 0
+    for (i, low) in maps.enumerated() {
+        if let j = Array(low).firstIndex(of: "S") {
+            startCoord = (i, j)
+        }
+        if let j = Array(low).firstIndex(of: "L") {
+            leverCoord = (i, j)
+        }
+        mapArr.append(Array(low))
+    }
+    
+    func bfs(x: Int, y: Int, target: Character) -> Int? {
+        var distance = Array(repeating: Array(repeating: -1, count: maps[0].count), count: maps.count)
         
-        var q = DoubleStackQueue<(Int, Int)>()
-        q.enqueue((x,y))
+        var q = [(Int, Int)]()
+        q.append((x,y))
+        distance[x][y] = 0
         
         while !q.isEmpty {
-            for _ in 0..<q.count { // 큐에 들어있는 좌표들의 개수만큼 반복문을 돌고 cnt를 1 증가시킨다. (그래야 한 레벨의 노드를 전부 처리하고 cnt를 증가시킬 수 있다.)
-                let first = q.dequeue()!
-                let x = first.0
-                let y = first.1
+            let (cx, cy) = q.removeFirst()
 
-                if maps[x][y] == target {
-                    return cnt
-                }
-                
-                var canMove = false
-
-                for i in 0..<4 {
-                    let nextX = x + dx[i]
-                    let nextY = y + dy[i]
-        
-                    if maps.indices ~= nextX && maps[0].indices ~= nextY && maps[nextX][nextY] != "X" {
-                        canMove = true
-                        if visited[nextX][nextY] == false {
-                            visited[x][y] = true
-                            q.enqueue((nextX, nextY))
-                        }
-                    }
-                }
-                
-                if canMove == false {
-                    return -1
-                }
+            if mapArr[cx][cy] == target {
+                return distance[cx][cy]
             }
             
-            cnt += 1
+            for i in 0..<4 {
+                let nextX = cx + dx[i]
+                let nextY = cy + dy[i]
+    
+                if isInRange(x: nextX, y: nextY) && mapArr[nextX][nextY] != "X" && distance[nextX][nextY] == -1 {
+                    distance[nextX][nextY] = distance[cx][cy] + 1
+                    q.append((nextX, nextY))
+                }
+            }
         }
         
-        return cnt
+        return nil
     }
     
-    var startCoor: (x: Int, y: Int) = (0, 0)
-    var leverCoor: (x: Int, y: Int) = (0, 0)
-    
-    for x in maps.indices {
-        for y in maps[0].indices {
-            if maps[x][y] == "S" {
-                startCoor = (x, y)
-            }
-            
-            if maps[x][y] == "L" {
-                leverCoor = (x, y)
-            }
+    func isInRange(x: Int, y: Int) -> Bool {
+        if x >= 0 && x < maps.count && y >= 0 && y < maps[0].count {
+            return true
         }
+        return false
     }
     
+    guard let cntToLever = bfs(x: startCoord.x, y: startCoord.y, target: "L") else { return -1 }
+    guard let cntToExit = bfs(x: leverCoord.x, y: leverCoord.y, target: "E") else { return -1 }
     
-    let cntToLever = bfs(x: startCoor.x, y: startCoor.y, target: "L", visited: visited1)
-    if cntToLever == -1 { return -1 }
-    let cntToEnd = bfs(x: leverCoor.x, y: leverCoor.y, target: "E", visited: visited2)
-    if cntToEnd == -1 { return -1 }
-    
-    return cntToLever + cntToEnd
+    return cntToLever + cntToExit
 }
