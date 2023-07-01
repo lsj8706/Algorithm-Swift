cbf3c9c3fb376e8b7e8e76efdd059aaa91c9613d
Sejin Lee <lsj8706@naver.com>
2023-07-01 21:51:39 +0900

    [FIX] : pg 142085 디펜스 게임 (시간 초과 해결 - 최대힙 사용)

--- Programmers/programmers/programmers/Level2/pg_142085.swift
+++ Programmers/programmers/programmers/Level2/pg_142085.swift
@@ -10,50 +10,38 @@
 import Foundation
 
 func solve142085() {
-//    print(solution(7, 3, [4, 2, 4, 5, 3, 3, 1]))
-    print(solution(2, 4, [3, 3, 3, 3]))
+    print(solution(7, 3, [4, 2, 4, 5, 3, 3, 1]))
+//    print(solution(2, 4, [3, 3, 3, 3]))
 }
 
 fileprivate func solution(_ n:Int, _ k:Int, _ enemy:[Int]) -> Int {
-    var result = 0
- 
-    func canDefence(index: Int) -> Bool {
-        let enemySlice = enemy[0...index].sorted(by: >)
-        var soldiers = n
+    var result = enemy.count
+    var n = n
+    var k = k
+    
+    var heap = Heap<Int>(sortFunction: { $0 > $1 })
+    
+    for (i, enemyCnt) in enemy.enumerated() {
+        heap.insert(node: enemyCnt)
         
-        for (i, enemyCnt) in enemySlice.enumerated() {
-            if i < k { continue }   // 무적권 사용
-            
-            soldiers -= enemyCnt
-            
-            if soldiers < 0 {
-                return false
+        n -= enemyCnt
+
+        if n - enemyCnt < 0 {
+            while k > 0 && n < 0 {
+                // 무적권 사용 (이전에 지나온 라운드에도 무적권 사용한 것으로 소급적용 가능)
+                let pastMax = heap.remove()!
+                n += pastMax
+                k -= 1
             }
         }
         
-        return true
-    }
-    
-    var left = 0
-    var right = enemy.count
-    
-    while left < right {
-        let mid = (left + right) / 2
-        
-        if canDefence(index: mid) {
-            left = mid + 1
-        } else {
-            right = mid
+        if n < 0 {
+            result = i
+            break
         }
     }
-    
-    result = left
     
     return result
 }
 
-
-
-//  참고: https://ksb-dev.tistory.com/257
-//  파라메트릭 서치(Parametric Search)
-//  이분 탐색을 통해 범위를 좁혀 나가며 해답을 찾는다. (이 문제에서는 라운드의 범위를 좁혀 나간다.)
+//  참고: https://school.programmers.co.kr/questions/43442
