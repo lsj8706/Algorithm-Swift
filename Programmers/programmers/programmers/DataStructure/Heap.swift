ba5ca4482a36a1fe572faea8e3b274cd4113158a
Sejin Lee <lsj8706@naver.com>
2023-07-01 21:51:16 +0900

    [FEAT] : Heap 추가

---
+++ Programmers/programmers/programmers/DataStructure/Heap.swift
@@ -0,0 +1,102 @@
+//
+//  Heap.swift
+//  programmers
+//
+//  Created by sejin on 2023/07/01.
+//
+
+import Foundation
+
+/// MaxHeap.
+/// MinHeap으로 사용하고 싶다면 음수로 변환하고 insert 할 것.
+public struct Heap<T: Comparable> {
+    private var elements: [T] = []
+    private let sortFunction: (T, T) -> Bool
+    
+    var isEmpty: Bool {
+        return self.elements.count == 1
+    }
+    var peek: T? {
+        if self.isEmpty { return nil }
+        return self.elements.last!
+    }
+    var count: Int {
+        return self.elements.count - 1
+    }
+    
+    init(elements: [T] = [], sortFunction: @escaping (T, T) -> Bool) {
+        if !elements.isEmpty {
+            self.elements = [elements.first!] + elements
+        } else {
+            self.elements = elements
+        }
+        self.sortFunction = sortFunction
+        if elements.count > 1 {
+            self.buildHeap()
+        }
+    }
+    
+    private func leftChild(of index: Int) -> Int {
+        return index * 2
+    }
+    
+    private func rightChild(of index: Int) -> Int {
+        return index * 2 + 1
+    }
+    
+    private func parent(of index: Int) -> Int {
+        return (index) / 2
+    }
+    
+    private mutating func add(element: T) {
+        self.elements.append(element)
+    }
+    
+    mutating private func diveDown(from index: Int) {
+        var higherPriority = index
+        let leftIndex = self.leftChild(of: index)
+        let rightIndex = self.rightChild(of: index)
+        
+        if leftIndex < self.elements.endIndex && self.sortFunction(self.elements[leftIndex], self.elements[higherPriority]) {
+            higherPriority = leftIndex
+        }
+        if rightIndex < self.elements.endIndex && self.sortFunction(self.elements[rightIndex], self.elements[higherPriority]) {
+            higherPriority = rightIndex
+        }
+        if higherPriority != index {
+            self.elements.swapAt(higherPriority, index)
+            self.diveDown(from: higherPriority)
+        }
+    }
+    
+    mutating private func swimUp(from index: Int) {
+        var index = index
+        while index != 1 && self.sortFunction(self.elements[index], self.elements[self.parent(of: index)]) {
+            self.elements.swapAt(index, self.parent(of: index))
+            index = self.parent(of: index)
+        }
+    }
+    
+    mutating private func buildHeap() {
+        for index in (1...(self.elements.count / 2)).reversed() {
+            self.diveDown(from: index)
+        }
+    }
+    
+    mutating func insert(node: T) {
+        if self.elements.isEmpty {
+            self.elements.append(node)
+        }
+        self.elements.append(node)
+        self.swimUp(from: self.elements.endIndex - 1)
+    }
+    
+    mutating func remove() -> T? {
+        if self.isEmpty { return nil }
+        self.elements.swapAt(1, elements.endIndex - 1)
+        let deleted = elements.removeLast()
+        self.diveDown(from: 1)
+        
+        return deleted
+    }
+}
