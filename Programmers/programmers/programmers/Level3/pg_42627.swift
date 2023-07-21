efddf5cd677b87a0285420495a291fa9d997535a
Lee SeJin <lsj8706@naver.com>
2023-07-21 17:23:35 +0900

    [Chore] : 주석 추가

--- Programmers/programmers/programmers/Level3/pg_42627.swift
+++ Programmers/programmers/programmers/Level3/pg_42627.swift
@@ -15,11 +15,11 @@ func solve42627() {
 }
 
 fileprivate func solution(_ jobs:[[Int]]) -> Int {
-    var jobs = jobs.sorted(by: { $0[0] >= $1[0] })
+    var jobs = jobs.sorted(by: { $0[0] >= $1[0] })  // 작업 요청 시점을 내림차순으로 정렬 (시간 복잡도가 낮은 removeLast를 사용하기 위해)
     let count = jobs.count
     var currentTime = 0
     var heap = Heap<Job>(sortFunction: <)
-    var totalTime = 0
+    var totalTime = 0   // 각 작업이 요청부터 종료까지 걸린 시간들의 합
     
     while !heap.isEmpty || !jobs.isEmpty {
         while !jobs.isEmpty && jobs.last![0] <= currentTime {
@@ -32,7 +32,7 @@ fileprivate func solution(_ jobs:[[Int]]) -> Int {
             totalTime += endTime - nextJob.inputTime
             
             currentTime = endTime
-        } else {
+        } else {    // Heap이 비어있다면 현재 시각에서 가장 가까운 작업을 찾아서 헤당 작업의 요청 시각으로 이동
             currentTime = jobs.last![0]
         }
     }
@@ -41,8 +41,8 @@ fileprivate func solution(_ jobs:[[Int]]) -> Int {
 }
 
 fileprivate struct Job: Comparable {
-    var inputTime: Int
-    var requiredTime: Int
+    var inputTime: Int      // 작업이 요청되는 시점
+    var requiredTime: Int   // 작업의 소요 시간
     
     static func < (lhs: Job, rhs: Job) -> Bool {
         if lhs.requiredTime == rhs.requiredTime {
