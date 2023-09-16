2b53fab78804521bdf47dc447b099795e253573c
Lee SeJin <lsj8706@naver.com>
2023-09-16 15:56:05 +0900

    [Fix] : pg 150366 (부분 해결)

--- Programmers/programmers/programmers/Level3/pg_150366.swift
+++ Programmers/programmers/programmers/Level3/pg_150366.swift
@@ -13,7 +13,7 @@ func solve150366() {
 //    print(solution(["UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"]))
     
 //    print(solution(["UPDATE 1 1 A", "UPDATE 2 2 B", "UPDATE 3 3 C", "UPDATE 4 4 D", "MERGE 1 1 2 2", "MERGE 3 3 4 4", "MERGE 1 1 3 3", "UNMERGE 1 1", "PRINT 1 1", "PRINT 2 2", "PRINT 3 3", "PRINT 4 4"]))
-    print(solution(["UPDATE 1 1 menu", "MERGE 1 1 1 2", "MERGE 1 1 1 3", "MERGE 1 1 1 4", "MERGE 1 2 1 3", "UPDATE 1 1 hansik", "PRINT 1 1", "PRINT 1 2", "PRINT 1 3", "PRINT 1 4"]))
+    print(solution(["MERGE 1 1 2 2", "MERGE 1 1 3 3", "UPDATE 3 3 A", "PRINT 1 1", "PRINT 2 2", "PRINT 3 3"]))
 }
 
 fileprivate func solution(_ commands:[String]) -> [String] {
@@ -56,7 +56,8 @@ fileprivate func solution(_ commands:[String]) -> [String] {
             let router2 = table[r2][c2]
             
             if (r1, c1) == (r2, c2) { break }
-//            if router1.value == router2.value { break }
+            
+            
             
             if router1.hasValue && router2.hasValue {
                 for (x, y) in router2.inboundCoords {
@@ -73,6 +74,9 @@ fileprivate func solution(_ commands:[String]) -> [String] {
                     table[x][y] = router2
                 }
                 router2.inboundCoords.append(contentsOf: router1.inboundCoords)
+            } else {
+                table[r2][c2] = router1
+                router1.inboundCoords.append((r2, c2))
             }
             
         case "UNMERGE":
