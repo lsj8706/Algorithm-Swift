44d5e18edbca66f358769528f5540a8eae01b0f3
Lee SeJin <lsj8706@naver.com>
2023-11-27 16:39:47 +0900

    [Fix] : pg 87377 - 범위 수정 및 y 좌표 구하는 방식 변경

--- Programmers/programmers/programmers/Level2/pg_87377.swift
+++ Programmers/programmers/programmers/Level2/pg_87377.swift
@@ -11,11 +11,44 @@ import Foundation
 
 func solve87377() {
 //    print(solution([[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]))
-    print(solution(    [[0, 1, -1], [1, 0, -1], [1, 0, 1]]))
+//    print(solution(    [[0, 1, -1], [1, 0, -1], [1, 0, 1]]))
+    print(solution([[1, -1, 0], [2, -1, 0]]))
 }
 
 fileprivate func solution(_ line:[[Int]]) -> [String] {
     var points = [(Int, Int)]()
+    
+    var leftX = Int.max
+    var rightX = Int.min
+    var topY = Int.min
+    var bottomY = Int.max
+    
+    func getPointOfIntersection(line1: [Int], line2: [Int]) -> (Int, Int)? {
+        let line1 = line1.map { Double($0) }
+        let line2 = line2.map { Double($0) }
+        let a = line1[0]
+        let b = line1[1]
+        let c = line1[2]
+        let A = line2[0]
+        let B = line2[1]
+        let C = line2[2]
+        
+        if (-A*b + a*B) == 0 { return nil }
+        
+        let x: Double = (-B*c + C*b) / (-A*b + a*B)
+        let y: Double = (c*A - a*C) / (-A*b + a*B)
+        
+        if let intX = Int(exactly: x), let intY = Int(exactly: y) {
+            leftX = min(leftX, intX)
+            rightX = max(rightX, intX)
+            topY = max(topY, intY)
+            bottomY = min(bottomY, intY)
+            
+            return (intX, intY)
+        }
+        
+        return nil
+    }
     
     for i in 0..<line.count-1 {
         for j in (i+1)..<line.count {
@@ -25,22 +58,15 @@ fileprivate func solution(_ line:[[Int]]) -> [String] {
         }
     }
     
-    var maxX = 0
-    var maxY = 0
+    let xLen = rightX - leftX
+    let yLen = topY - bottomY
     
-    for point in points {
-        let absX = abs(point.0)
-        let absY = abs(point.1)
-        
-        maxX = max(maxX, absX)
-        maxY = max(maxY, absY)
-    }
-        
-    var board = Array(repeating: Array(repeating: ".", count: 2*maxX+1), count: 2*maxY+1)
+    var board = Array(repeating: Array(repeating: ".", count: xLen+1), count: yLen+1)
     
+    // 2차원 배열의 중심점을 (0,0) 좌표로 변환
     for point in points {
-        let x = point.0 + maxX
-        let y = abs(point.1 - maxY)
+        let x = point.0 - leftX
+        let y = topY - point.1
         
         board[y][x] = "*"
     }
@@ -48,32 +74,4 @@ fileprivate func solution(_ line:[[Int]]) -> [String] {
     let result = board.map { $0.joined() }
     
     return result
-}
-
-fileprivate func getPointOfIntersection(line1: [Int], line2: [Int]) -> (Int, Int)? {
-    let line1 = line1.map { Double($0) }
-    let line2 = line2.map { Double($0) }
-    let a = line1[0]
-    let b = line1[1]
-    let c = line1[2]
-    let A = line2[0]
-    let B = line2[1]
-    let C = line2[2]
-    
-    var x: Double = 0
-    var y: Double = 0
-    
-    if -B*c + C*b != 0 {
-        x = (-B*c + C*b) / (-A*b + a*B)
-    }
-    
-    if -a*x - c != 0 {
-        y = (-a*x - c) / b
-    }
-    
-    if let intX = Int(exactly: x), let intY = Int(exactly: y) {
-        return (intX, intY)
-    }
-    
-    return nil
 }
