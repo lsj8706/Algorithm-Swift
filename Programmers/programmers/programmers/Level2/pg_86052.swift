6ef2d2b123a0a97f8a1376a5323baa414f84e5cb
Lee SeJin <lsj8706@naver.com>
2024-01-02 11:33:11 +0900

    [Fix] : pg 86052 시간 초과 해결

--- Programmers/programmers/programmers/Level2/pg_86052.swift
+++ Programmers/programmers/programmers/Level2/pg_86052.swift
@@ -17,7 +17,7 @@ func solve86052() {
 fileprivate func solution(_ grid:[String]) -> [Int] {
     let grid = grid.map { Array($0).map { String($0) } }
     var results = [Int]()
-    var routes = Set<[Int]>() // [x1,y1,x2,y2,direction]
+    var routes = Set<[Int]>() // [x,y,direction]
     
     func getNextPosition(x: Int, y: Int, from direction: Direction, node: String) -> (Int, Int, Direction) {
         var nextX = x
@@ -101,32 +101,30 @@ fileprivate func solution(_ grid:[String]) -> [Int] {
         return (nextX, nextY, dir)
     }
     
-    func findRoute(x: Int, y: Int, from: Direction, count: Int, now: Set<[Int]>) {
-        var now = now
+    func findRoute(x: Int, y: Int, from: Direction, count: Int) {
         let node = grid[x][y]
         let (nextX, nextY, direction) = getNextPosition(x: x, y: y, from: from, node: node)
         
-        let edge = [x, y, nextX, nextY, from.rawValue]
-        // 이미 계산한 경로인 경우 return
-        if routes.contains(edge) {
-            return
-        }
+        let edge = [nextX, nextY, direction.rawValue]
         
         // 하나의 경로를 다 탐색한 경우
-        if now.contains(edge) {
-            results.append(count)
-            routes = routes.union(now)
+        if routes.contains(edge) {
+            if count > 0 {
+                results.append(count)
+            }
             return
         }
         
-        now.insert(edge)
-        findRoute(x: nextX, y: nextY, from: direction, count: count+1, now: now)
+        routes.insert(edge)
+        findRoute(x: nextX, y: nextY, from: direction, count: count+1)
     }
     
     for x in grid.indices {
         for y in grid[0].indices {
             for direction in Direction.allCases {
-                findRoute(x: x, y: y, from: direction, count: 0, now: [])
+                if !routes.contains([x, y, direction.rawValue]) {
+                    findRoute(x: x, y: y, from: direction, count: 0)
+                }
             }
         }
     }
