aba3cc77f8bb987c8cb2443555f81348ed113ff9
Lee SeJin <lsj8706@naver.com>
2024-02-01 18:25:15 +0900

    [Chore] : 주석추가

--- Programmers/programmers/programmers/Level3/pg_181186.swift
+++ Programmers/programmers/programmers/Level3/pg_181186.swift
@@ -32,7 +32,7 @@ fileprivate func solution(_ n:Int) -> Int {
   for i in 4...n {
     dp[i] += dp[i-1] + dp[i-2]*2 + dp[i-3]*5
     sum[(i-4)%3] = (sum[(i-4)%3] + dp[i-4]) % mod
-    dp[i] += (sum[0] + sum[1] + sum[2] + sum[i%3])*2
+    dp[i] += (sum[0] + sum[1] + sum[2] + sum[i%3])*2 // sum[i%3]을 더해주는 이유는 2,2,4의 반복에서 4의 차례일 때가 i%3 번째이기 때문에 한번 더 더해줘서 4를 만들어주는 것이다.
     dp[i] %= mod
   }
   
